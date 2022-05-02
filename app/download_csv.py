2e7044a9b0051c3f140b562911faa8a5d3e2a3db
jordan martin <devmartinjordan@gmail.com>
2022-05-02 17:25:58 +0200

    fix: path to create

--- app/download_csv.py
+++ app/download_csv.py
@@ -13,9 +13,9 @@ while year_counter > 2007:
     data = r.text
     soup = BeautifulSoup(data, features="html5lib")
     for link in soup.find_all('a'):
-        os.makedirs(f"/data/{year_counter}", exist_ok=True)
+        os.makedirs(f"./data/{year_counter}", exist_ok=True)
         print(f"DIRECTORY {year_counter} CREATED")
         if link.get('href').endswith(".csv"):
-            urllib.request.urlretrieve(url_dir + link.string, filename=f"/data/{year_counter}/{link.string}")
+            urllib.request.urlretrieve(url_dir + link.string, filename=f"./data/{year_counter}/{link.string}")
             print(f"DOWNLOADED: {url_dir + link.string}")
     year_counter -= 1
