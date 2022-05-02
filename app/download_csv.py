81eddd8a2a2cc2203b5b96f1a2484aa11223a87b
jordan martin <devmartinjordan@gmail.com>
2022-05-02 17:02:06 +0200

    fix: create directory by year

---
+++ app/download_csv.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+
+year_counter = 1901
+
+while year_counter < 2023:
+    url_dir = f"https://www.ncei.noaa.gov/data/global-hourly/access/{year_counter}/"
+    r  = requests.get(url_dir)
+    data = r.text
+    soup = BeautifulSoup(data, features="html5lib")
+    for link in soup.find_all('a'):
+        try:
+            os.makedirs(f"/data/{year_counter}", exist_ok=True)
+            print(f"DIRECTORY {year_counter} CREATED")
+        except FileExistsError:
+            continue
+        if link.get('href').endswith(".csv"):
+            urllib.request.urlretrieve(url_dir + link.string, filename=f"/data/{year_counter}/{link.string}")
+            print(f"DOWNLOADED: {url_dir + link.string}")
+    year_counter += 1
