bbd991a1862e9f1618c16bdb9ea39d09b1914228
vittorio-c <dev@tov.mozmail.com>
2022-05-04 16:31:55 +0200

    Added select stations by year with geocordinates

---
+++ frontend/src/components/ShowStationsByYear.vue
@@ -0,0 +1,106 @@
+<template>
+  <h2>Températures moyennes par stations pour {{ selectedYear }}</h2>
+  <p>Choisir une autre année : </p>
+
+  <select v-if="distinctYears" v-model="selectedYear">
+    <option v-for="(year, key) in distinctYears" v-bind:key="key">
+      {{ year }}
+    </option>
+  </select>
+  <div
+    style="height: 75vh; width: 75vw;"
+  >
+    <l-map
+      v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      @move="log('move')"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-control-layers/>
+      <div v-if="showCoordinates">
+        <l-marker
+          v-for="coordinate in geocordinates"
+          v-bind:key="coordinate.stations_id"
+          :lat-lng="[ coordinate.latitude, coordinate.longitude]"
+          @moveend="log('moved marker')"
+        >
+          <l-tooltip>
+            Température moyenne : {{ new Intl.NumberFormat().format(coordinate.mean_tmp) }} ° C
+          </l-tooltip>
+        </l-marker>
+      </div>
+    </l-map>
+  </div>
+</template>
+<script>
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+  LTooltip,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+import axios from "axios";
+
+export default {
+  name: "show-stations-by-year",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LControlLayers,
+    LTooltip,
+  },
+  data() {
+    return {
+      zoom: 2,
+      coordinates: [50, 50],
+      iconWidth: 25,
+      iconHeight: 40,
+      geocordinates: [],
+      year: 2018,
+      distinctYears: [],
+      selectedYear: 2018,
+    };
+  },
+  computed: {
+    path() {
+      return "http://localhost:8088/api/geo/by-year/" + this.selectedYear
+    },
+    showCoordinates() {
+      return Object.keys(this.coordinates).length > 0
+    }
+  },
+  watch: {
+    selectedYear() {
+      this.fetchApiData()
+    }
+  },
+  methods: {
+    log(a) {
+      console.log(a);
+    },
+    fetchApiData() {
+      axios
+        .get(this.path)
+        .then((response) => {
+          this.geocordinates = response.data._data;
+          this.distinctYears = response.data._meta.distinct_years;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.fetchApiData()
+  },
+};
+</script>
+
+<style></style>
